e674829b3d39010310404f364acbc6042a54115d
Dmitry Baranov <zyzychka1@gmail.com>
2023-01-29 02:30:36 +0300

    test

--- src/components/registrationForm/registrationForm.js
+++ src/components/registrationForm/registrationForm.js
@@ -6,7 +6,7 @@ import './registrationForm.css';
 const RegistrationForm = () => {
 	const {tg} = useTelegram();
 	const [fio, setFio] = useState('');
-	const [adress, setadress] = useState('');
+	const [adress, setAdress] = useState('');
 	const [phoneNumber, setPhoneNumber] = useState('');
 	const [email, setEmail] = useState('');
 	const [validEmail, setValidEmail] = useState('');
@@ -14,6 +14,19 @@ const RegistrationForm = () => {
 	const onChangeEmail = (e) => {
 		setEmail(e.target.value);
 	}
+
+	const onChangeFio = (e) => {
+		setFio(e.target.value);
+	}
+
+	const onChangeAdress = (e) => {
+		setAdress(e.target.value);
+	}
+
+	const onChangePhoneNumber = (e) => {
+		setPhoneNumber(e.target.value);
+	}
+
 
 	const onBlurEmail = (e) => {
 		let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -30,12 +43,12 @@ const RegistrationForm = () => {
 				<Form.Control className={'input is-' + validEmail} type='email' placeholder='name@example.com' value={email} onChange={onChangeEmail} onBlur={onBlurEmail}/>
 				<Form.Control.Feedback type='invalid'>Пожалуйста, укажите корректный email</Form.Control.Feedback>
 			</FloatingLabel>
-			<FloatingLabel label='Ваш email' controlId="floatingInput" className="mb-3">
-				<Form.Control className={'input is-' + validEmail} type='email' placeholder='name@example.com' value={email} onChange={onChangeEmail} onBlur={onBlurEmail}/>
+			<FloatingLabel label='Ваше ФИО' controlId="floatingInput" className="mb-3">
+				<Form.Control className={'input'} type='text' placeholder='Иванов Иван Иванович' value={fio} onChange={onChangeFio} />
 				<Form.Control.Feedback type='invalid'>Пожалуйста, укажите корректный email</Form.Control.Feedback>
 			</FloatingLabel>
-			<FloatingLabel label='Ваш email' controlId="floatingInput" className="mb-3">
-				<Form.Control className={'input is-' + validEmail} type='email' placeholder='name@example.com' value={email} onChange={onChangeEmail} onBlur={onBlurEmail}/>
+			<FloatingLabel label='Ваш адрес доставки' controlId="floatingInput" className="mb-3">
+				<Form.Control className={'input'} type='adress' placeholder='name@example.com' value={email} onChange={onChangeEmail} onBlur={onBlurEmail}/>
 				<Form.Control.Feedback type='invalid'>Пожалуйста, укажите корректный email</Form.Control.Feedback>
 			</FloatingLabel>
 			<FloatingLabel label='Ваш email' controlId="floatingInput" className="mb-3">
