adb2c9bf99ff238e2c8141f06b3ba62cb69d40ac
Dmitry Baranov <zyzychka1@gmail.com>
2023-01-29 01:09:04 +0300

    test

--- src/components/registrationForm/registrationForm.js
+++ src/components/registrationForm/registrationForm.js
@@ -1,26 +1,34 @@
 import React, { useState } from 'react';
-import { Button, FloatingLabel, Form } from 'react-bootstrap';
+import { Button, FloatingLabel, Form, InputGroup } from 'react-bootstrap';
 
 const RegistrationForm = () => {
 	const [fio, setFio] = useState('');
 	const [adress, setadress] = useState('');
 	const [phoneNumber, setPhoneNumber] = useState('');
 	const [email, setEmail] = useState('');
-	const [valid, setValid] = useState('');
+	const [validEmail, setValidEmail] = useState('');
 
 	const onChangeEmail = (e) => {
+		setEmail(e.target.value);
+	}
+
+	const onBlurEmail = (e) => {
 		let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		if(re.test(e.target.value)){
-			
-		} 
-		setEmail(e.target.value);
+			setValidEmail('valid')
+		} else {
+			setValidEmail('invalid')
+		}
 	}
 
 	return (
 		<div className='reg'>
-			<FloatingLabel label='Ваш email' controlId="floatingInput" className="mb-3">
-				<Form.Control className='input' type='email' placeholder='name@example.com' value={email} onChange={onChangeEmail} isValid={false}/>
-			</FloatingLabel>
+			<InputGroup hasValidation>
+				<FloatingLabel label='Ваш email' controlId="floatingInput" className="mb-3">
+					<Form.Control className={'input is-' + validEmail} type='email' placeholder='name@example.com' value={email} onChange={onChangeEmail} onBlur={onBlurEmail}/>
+					<Form.Control.Feedback type='invalid'>Пожалуйста, укажите корректный email</Form.Control.Feedback>
+				</FloatingLabel>
+			</InputGroup>
 		</div>
 	);
 };
