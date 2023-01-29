88d8364bfff23a38dfbf1a486c5b2b0d75060304
Dmitry Baranov <zyzychka1@gmail.com>
2023-01-29 17:55:57 +0300

    test

--- src/components/registrationForm/registrationForm.js
+++ src/components/registrationForm/registrationForm.js
@@ -78,7 +78,9 @@ const RegistrationForm = () => {
 				<Form.Control className={'input'} type='text' placeholder='Иванов Иван Иванович' value={fio} onChange={onChangeFio} />
 				<AddressSuggestions 
 					token='22cd6c7adac9d78ce2cb0559940b208f26701947' 
-					inputProps={{placeholder: 'Адрес', className:'react-dadata__input input form-control', value: adress, onChange: onChangeAdress}} 
+					inputProps={{placeholder: 'Адрес', className:'react-dadata__input input form-control'}}
+					value={adress}
+					onChange={onChangeAdress} 
 					suggestionsClassName='suggestion' 
 				/>
 				
