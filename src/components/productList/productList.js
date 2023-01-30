a8b8fc1dcfc31740bc024543ab9926cebeaa2bfc
Dmitry Baranov <zyzychka1@gmail.com>
2023-01-30 20:02:25 +0300

    test

--- src/components/productList/productList.js
+++ src/components/productList/productList.js
@@ -27,7 +27,7 @@ export default class ProductList extends React.Component{
 	}
 
 	componentDidMount(){
-		this.tg.onEvent('mainButtonClicked', this.onSendData);
+		this.tg.onEvent('mainButtonClicked', this.onSendData());
 	}
 
 	componentDidMount() {
