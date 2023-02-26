4fb5d7c3d31480972650f36ecee4dd621428e0bf
Dmitry Baranov <zyzychka1@gmail.com>
2023-02-26 14:34:35 +0300

    redux persist

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { persistor, store } from './store';
 import ProductsServiceContext from './components/productsContext/productsServiceContext';
 import ProductsService from './services/productsService';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const producsService = new ProductsService();
 
@@ -13,9 +14,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ProductsServiceContext.Provider value={producsService}>
-        <App />
-      </ProductsServiceContext.Provider>
+      <PersistGate persistor={persistor}>
+        <ProductsServiceContext.Provider value={producsService}>
+          <App />
+        </ProductsServiceContext.Provider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store.js
+++ src/store.js
@@ -1,4 +1,15 @@
 import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { reducer } from "./reducers";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
 
-export const store = createStore(reducer);
+const persistConfig = {
+	key: 'root',
+	storage: storage,
+	stateReconciler: autoMergeLevel2
+};
+
+const pReducer = persistReducer(persistConfig, reducer);
+export const store = createStore(pReducer);
+export const persistor = persistStore(store);
