dac4440bcbf965b424b0321c6d3dae64f4555259
Dmitry Baranov <zyzychka1@gmail.com>
2023-03-13 20:57:13 +0300

    admin impl

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -63,34 +63,115 @@ const AdminItem = ({item}) => {
 }
 
 export const AddItem = () => {
-	const [colors, setColors] = useState([{
-		name: '',
+	const [colors, setColors] = useState([]);
+	const [photos, setPhotos] = useState([{
 		src: ''
-	}]);
+	}])
+	const [title, setTitle] = useState(null);
+	const [type, setType] = useState(null);
+	const [descr, setDescr] = useState(null);
+	const [sizes, setSizes] = useState([]);
+	const [price, setPrice] = useState(null);
 
 	return (
 		<form className='admin-form' onSubmit={(e) => {
-			console.log(colors);
+			e.preventDefault();
+			let data = new FormData();
+			data.append('title', title);
+			data.append('type', type);
+			data.append('descr', descr);
+			data.append('price', price);
+
+			sizes.map(item => {
+				data.append('sizes', item);
+			})
+			colors.map(item => {
+				data.append('colorNames', item.name);
+				data.append('colorSrcs', item.src);
+			})
+			photos.map(item => {
+				data.append('files', item.src);
+			})
+
+			// console.log(data.getAll('title'));
+			// console.log(data.getAll('type'));
+			// console.log(data.getAll('descr'));
+			// console.log(data.getAll('price'));
+			// console.log(data.getAll('sizes'));
+			// console.log(data.getAll('colorNames'));
+			// console.log(data.getAll('colorSrcs'));
+			// console.log(data.getAll('files'));
+			fetch('http://95.31.1.120:5000',{
+				method: 'POST',
+				body: data
+			})
 		}}>
 			<label htmlFor='title'>Название</label>
-			<input type='text' name='title' required/>
+			<input type='text' name='title' required onChange={(e) => setTitle(e.target.value)}/>
 			
 			<label htmlFor='type'>Тип</label>
-			<input type='text' name='type'/>
+			<input type='text' name='type' required onChange={(e) => setType(e.target.value)}/>
 
 			<label htmlFor='descr'>Описание</label>
-			<input type='text' name='descr'/>
+			<textarea type='text' name='descr' required onChange={(e) => setDescr(e.target.value)}/>
 
 			<details>
 				<summary>Цвета</summary>
-				
+				{colors.map((item, index) => (
+					<div className='admin-color' key={index}>
+						<label htmlFor='name'>Имя</label>
+						<input name='name' type='text' value={colors[index].name || ''} required onChange={(e) => {
+							let data = [...colors];
+							data[index].name = e.target.value;
+							setColors(data);
+						}}/>
+
+						<label className='file' htmlFor={"filePicker"+index}>{colors[index].src?.name || 'Выберите файл'}</label>
+						<input id={"filePicker"+index} type={"file"} hidden required onChange={(e) => {
+							let data = [...colors];
+							data[index].src = e.target.files[0];
+							setColors(data);
+						}}/>
+						<button type='button' onClick={() => {
+							let data = [...colors];
+							data.splice(index, 1);
+							setColors(data);
+						}}>Удалить</button>
+					</div>
+				))}
+				<button type='button' onClick={() => setColors([...colors, {name: null, src: null}])}>Добавить цвет</button>
+			</details>
+
+			<details>
+				<summary>Фото</summary>
+				{photos.map((item, index) => (
+					<div className='admin-color' key={'photo'+index}>
+						<label className='file' htmlFor={"photoPicker"+index}>{photos[index].src?.name || 'Выберите файл'}</label>
+						<input id={"photoPicker"+index} type={"file"} hidden required onChange={(e) => {
+							let data = [...photos];
+							data[index].src = e.target.files[0];
+							setPhotos(data);
+						}}/>
+						{index !== 0 ? (
+							<button type='button' onClick={() => {
+								let data = [...photos];
+								data.splice(index, 1);
+								setPhotos(data);
+							}}>Удалить</button>
+						) : null}
+					</div>
+				))}
+				<button type='button' onClick={() => setPhotos([...photos, {src: null}])}>Добавить фото</button>
 			</details>
 
 			<label htmlFor='sizes'>Размеры</label>
-			<input type='text' name='sizes'/>
+			<input type='text' name='sizes' onChange={(e) => {
+					const data = e.target.value.split(' ');
+					setSizes(data);
+				}}/>
 
 			<label htmlFor='price'>Цена</label>
-			<input type='number' name='price'/>
+			<input type='number' name='price' required onChange={(e) => setPrice(e.target.value)}/>
 
 			<button type="submit" >Добавить</button>
 		</form>
