48af86000c9f8782a1b9d24dabfb8b6cd9dad4df
Dmitry Baranov <zyzychka1@gmail.com>
2023-03-15 15:10:39 +0300

    admin

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -1,6 +1,7 @@
 export default class ProductsService {	
 	getAll = async () => {
 		const res = await fetch(`https://dmnsmgn.ru/api`);
+		console.log(await res.json());
 		
 		return (await res.json());
 	}
