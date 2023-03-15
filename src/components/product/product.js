3198bafb72ee267b060f9722763c9b6ebbe34b4b
Dmitry Baranov <zyzychka1@gmail.com>
2023-03-15 15:11:44 +0300

    admin

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -10,7 +10,7 @@ class Product extends React.Component{
 		const {title, src, price, id, sizes, colors} = this.props.product;
 		const exist = this.props.cart.find(item => item.id === id);
 		const {onInc, onDec} = this.props;
-		const count  = (sizes || colors.length !== 0) ? 0 : exist ? exist.quantity : 0;
+		const count  = 0;
 		// console.log(src);
 		return (
 			<div className='product-card-wrapper'>
